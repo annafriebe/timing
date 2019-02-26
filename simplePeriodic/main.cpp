2c0190e8604798c9136751b1402738aff5248f74
Anna Friebe <anna.friebe@ha.ax>
2019-02-26 15:02:40 +0200

    sleep time calculation, task

--- simplePeriodic/main.cpp
+++ simplePeriodic/main.cpp
@@ -6,21 +6,29 @@
 #include "Timer.h"
 
 const double period = 0.01;
+using namespace std;
 
 namespace{
-	void task(int& nPeriods){
-		nPeriods++;
+	void task(int& nPeriod, double& calc){
+		for(int i=1; i<=100; ++i){
+			calc +=nPeriod%i;
+		}
+		nPeriod++;
 	}
 }
 
 int main(int argc, char *argv[]) {
 	Timer timer;
-	int nPeriod = 0;
+	int nTaskCalls = 0;
+	double calc = 0;
 	timer.start();
+	double currentPeriod = period;
 	while(1){
-		task(nPeriod);
-		timer.sleepUntil(period);
-		timer.reset();
+		task(nTaskCalls, calc);
+		timer.sleepUntil(currentPeriod);
+		currentPeriod += period;
 	}
+	cerr << "nTaskCalls: " << nTaskCalls << endl;
+	cerr << "calc: " << calc << endl;
 	return 0;
 }
